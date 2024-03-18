5bdaf6e741d6ccf8a59bc9a8571238c9926f8318
Jonathan <jpreuven@gmail.com>
2024-03-18 15:11:09 -0700

    adding logos to sidebar

--- client/education-app/src/components/Sidebar/Sidebar.data.jsx
+++ client/education-app/src/components/Sidebar/Sidebar.data.jsx
@@ -1,9 +1,14 @@
 // import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import settingLogo from "../../../src/assets/setting-logo.svg";
+import profileLogo from "../../../src/assets/profile-logo.svg";
+import logoutLogo from "../../../src/assets/logout-logo.svg";
+import homeLogo from "../../../src/assets/home-logo.svg";
+import classLogo from "../../../src/assets/class-logo.svg";
 
 const SideBarData = [
-  { href: "/", title: "Home" },
-  { href: "/properties", title: "Properties" },
+  { href: "/", title: "Home", logo: homeLogo },
+  { href: "/classes", title: "Classes", logo: classLogo },
   { href: "/tenants", title: "Tenants" },
   { href: "/leases", title: "Leases" },
   { href: "/link5", title: "Link 5" },
@@ -27,14 +32,57 @@ const SideBarData = [
   { href: "/link5", title: "Link 5" },
 ];
 
-const SideBarDataJSX = SideBarData.map((link, index) => {
+const SideBarLowerData = [
+  {
+    href: "/settings",
+    title: "Settings",
+    logo: settingLogo,
+  },
+  { href: "/account", title: "Account Info", logo: profileLogo },
+  { href: "/", title: "Logout", logo: logoutLogo },
+];
+
+const SideBarDataJSX = SideBarData.map(({ href, title, logo }, index) => {
+  const sideBarLinkStyle = {
+    flexGrow: "2",
+    position: "relative",
+    zIndex: "-1",
+    left: "0",
+  };
+  if (!logo) {
+    sideBarLinkStyle["left"] = "-10px";
+  }
   return (
     <li key={index} className="sidebar-links">
       <div className="sidebar-links-div">
-        <Link to={`${link.href}`}>{link.title}</Link>
+        <Link to={`${href}`}>
+          <div style={{ display: "flex" }}>
+            <p style={sideBarLinkStyle}>{title}</p>
+            {logo ? <img src={logo} style={{ width: "20px" }} /> : null}
+          </div>
+        </Link>
       </div>
     </li>
   );
 });
 
-export { SideBarDataJSX };
+const SideBarLowerDataJSX = SideBarLowerData.map(
+  ({ href, title, logo }, index) => {
+    return (
+      <li key={index} className="sidebar-links">
+        <div className="sidebar-links-div">
+          <Link to={`${href}`}>
+            <div style={{ display: "flex" }}>
+              <p style={{ flexGrow: "2" }}>{title}</p>
+              <img src={logo} style={{ width: "20px" }} />
+            </div>
+          </Link>
+          {/* <div style={{ width: "1px" }}> */}
+          {/* </div> */}
+        </div>
+      </li>
+    );
+  }
+);
+
+export { SideBarDataJSX, SideBarLowerDataJSX };
--- client/education-app/src/components/Sidebar/Sidebar.jsx
+++ client/education-app/src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,36 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
 // import logo from "../../images/ProperlyManagementSolutionsLogo.jpg";
-import { useSelector } from "react-redux";
-import { SideBarDataJSX } from "./Sidebar.data";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { SideBarDataJSX, SideBarLowerDataJSX } from "./Sidebar.data";
+import { setUser } from "../../app/features/users/userSlice";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SideBar() {
   const mobileSidebarIsOpen = useSelector(
     (state) => state.mobileSidebarIsOpen.value
   );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    fetch("http://localhost:5000/logout", {
+      method: "DELETE",
+      credentials: "include",
+    })
+      .then((r) => {
+        if (r.ok) {
+          console.log("User logged out!");
+          //   navigate("/");
+          dispatch(setUser(null));
+        } else {
+          console.log("Something went wrong!");
+        }
+      })
+      .catch((error) => {
+        console.error("Error logging out:", error);
+      });
+  }
 
   return (
     <div className="sidebar-outer-div">
@@ -23,7 +45,7 @@ export default function SideBar() {
         </div>
         <ul className="sidebar-link-list">{SideBarDataJSX}</ul>
         <ul className="sidebar-link-list" id="sidebar-botton-link-list">
-          <li className="sidebar-links">
+          {/* <li className="sidebar-links">
             <div className="sidebar-links-div">
               <Link to="/testing">Settings</Link>
             </div>
@@ -33,6 +55,14 @@ export default function SideBar() {
               <Link to="/testing1">Account Info</Link>
             </div>
           </li>
+          <li className="sidebar-links">
+            <div className="sidebar-links-div">
+              <Link to="/" onClick={handleLogout}>
+                Logout
+              </Link>
+            </div>
+          </li> */}
+          {SideBarLowerDataJSX}
         </ul>
       </nav>
     </div>
