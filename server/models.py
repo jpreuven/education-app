06d0995d8f9499b49b906b47b87334e3ad5dd3d7
Jonathan <jpreuven@gmail.com>
2024-03-26 21:17:07 -0700

    clearing commit messages

--- server/models.py
+++ server/models.py
@@ -3,72 +3,6 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.ext.associationproxy import association_proxy
 
-
-# class User(db.Model, SerializerMixin):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     _password_hash = db.Column(db.String, nullable=False)
-
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-    
-#     @hybrid_property
-#     def password_hash(self):
-#         raise AttributeError("password hashes may not be viewed")
-
-#     @password_hash.setter
-#     def password_hash(self, password):
-#         password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
-#         self._password_hash = password_hash.decode("utf-8")
-
-#     def authenticate(self, password):
-#         return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
-
-# class User(db.Model, SerializerMixin):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     _password_hash = db.Column(db.String, nullable=False)
-#     first_name = db.Column(db.String(50), nullable=False)
-#     last_name = db.Column(db.String(50), nullable=False)
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-    
-#     @hybrid_property
-#     def password_hash(self):
-#         raise AttributeError("password hashes may not be viewed")
-
-#     @password_hash.setter
-#     def password_hash(self, password):
-#         password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
-#         self._password_hash = password_hash.decode("utf-8")
-
-#     def authenticate(self, password):
-#         return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
-
-#     serialize_rules = ('-password_hash',)
-
-# class Student(db.Model, SerializerMixin):
-#     __tablename__ = 'students'
-#     student_id = db.Column(db.Integer, primary_key=True)
-#     username_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     user = db.relationship('User', backref=db.backref('student', uselist=False))
-
-#     serialize_rules = ('-user._password_hash',)
-
-# class Teacher(db.Model, SerializerMixin):
-#     __tablename__ = 'teachers'
-#     teacher_id = db.Column(db.Integer, primary_key=True)
-#     username_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     user = db.relationship('User', backref=db.backref('teacher', uselist=False))
-
-#     serialize_rules = ('-user._password_hash',)
-
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     serialize_rules = ('-_password_hash', '-id')
@@ -154,7 +88,6 @@ class TeacherCourseAssociation(db.Model, SerializerMixin):
     serialize_rules = ('-teacher.teacher_course_association', '-course.teacher_course_association')
 
     teacher_course_association_id = db.Column(db.Integer, primary_key=True)
-    # section_title = db.Column(db.String(100), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.teacher_id'), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.course_id'), nullable=False)
     teacher = db.relationship("Teacher", back_populates="teacher_course_association")
